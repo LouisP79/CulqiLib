a67fae696b2fa4fb83262607a39333dd42da6e20
Louis Perdomo <louis.perdomo79@gmail.com>
2019-08-01 10:35:12 -0500

    definitive changes

--- culqilib/src/main/java/com/culqilib/Token.kt
+++ culqilib/src/main/java/com/culqilib/Token.kt
@@ -1,14 +1,14 @@
 package com.culqilib
 
-import android.annotation.SuppressLint
 import com.culqilib.extensions.errorResponse
 import com.culqilib.extensions.successResponse
 import com.culqilib.listener.TokenCallback
 import com.culqilib.restServices.TokenService
 import com.culqilib.restServices.deserializer.TokenDeserializer
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.rxkotlin.subscribeBy
-import io.reactivex.schedulers.Schedulers
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -19,7 +19,7 @@ import retrofit2.converter.jackson.JacksonConverterFactory
  */
 class Token(private var apiKey: String) {
 
-    @SuppressLint("CheckResult")
+
     fun createToken(card: TokenDeserializer.CardRequest, listener: TokenCallback){
         val retrofit: Retrofit = Retrofit.Builder()
                 .baseUrl("https://secure.culqi.com")
@@ -29,13 +29,12 @@ class Token(private var apiKey: String) {
 
         retrofit.create(TokenService::class.java)
                 .token("Bearer $apiKey",card)
-                .subscribeOn(Schedulers.single())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeBy(
-                        onNext = {  if(it.isSuccessful) listener.onSuccess(it.body()!!.successResponse())
-                                    else listener.onError(it.errorBody()!!.errorResponse())},
-                        onError = { it.printStackTrace() },
-                        onComplete = { println("onComplete!") }
-                )
+                .enqueue(object: Callback<ResponseBody>{
+                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {t.printStackTrace()}
+                    override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                        if(response.isSuccessful) listener.onSuccess(response.body()!!.successResponse())
+                        else listener.onError(response.errorBody()!!.errorResponse())
+                    }
+                })
     }
 }
